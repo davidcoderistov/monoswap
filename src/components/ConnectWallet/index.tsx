93ded814d92b4f12ab1ca3f5924a356d68a36673
davidcoderistov <davidcoderistov@gmail.com>
2022-09-12 13:47:24 +0200

    Create ConnectWallet component

---
+++ src/components/ConnectWallet/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+
+interface ConnectWalletProps {
+    onClick: () => void
+}
+
+export default function ConnectWallet (props: ConnectWalletProps) {
+
+    return (
+        <Box
+            component='div'
+            sx={{
+                display: 'inline-block',
+                backgroundColor: '#172A42',
+                color: '#467DCB',
+                borderRadius: '20px',
+                paddingY: '10px',
+                paddingX: '15px',
+                cursor: 'pointer',
+                '&:hover': {
+                    color: '#0073cf',
+                    backgroundColor: '#172A42',
+                }
+            }}
+            onClick={props.onClick}
+        >
+            <Box
+                display='flex'
+                alignItems='center'
+            >
+                <Box sx={{ height: 25 }} />
+                <Typography fontSize={16} fontWeight='bold'>Connect Wallet</Typography>
+            </Box>
+        </Box>
+    )
+}
