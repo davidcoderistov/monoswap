1f18f1eda62e9426bf71ca2e341d8532feb2c4b7
davidcoderistov <davidcoderistov@gmail.com>
2022-09-15 13:56:19 +0200

    Create ConnectWalletModal->WalletsView component

---
+++ src/components/ConnectWalletModal/WalletsView.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Box } from '@mui/material'
+import WalletButton, { WalletButtonProps } from './WalletButton'
+
+
+interface WalletsViewProps {
+    connectedTo: 'metamask' | 'walletconnect' | null
+    isMetamaskInstalled: boolean
+    onClick: WalletButtonProps['onClick']
+}
+
+export default function WalletsView (props: WalletsViewProps) {
+
+    return (
+        <Box marginX='20px'>
+            <Box mt='15px' />
+            <WalletButton
+                id='metamask'
+                title={props.isMetamaskInstalled ? 'MetaMask' : 'Install MetaMask'}
+                imgSrc='/metamask.png'
+                connected={props.connectedTo === 'metamask'}
+                onClick={props.onClick} />
+            <Box mb='10px' />
+            <WalletButton
+                id='walletconnect'
+                title='WalletConnect'
+                imgSrc='/walletconnect.png'
+                connected={props.connectedTo === 'walletconnect'}
+                onClick={props.onClick} />
+            <Box mt='15px' />
+        </Box>
+    )
+}
