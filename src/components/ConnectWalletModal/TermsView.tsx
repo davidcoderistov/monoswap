c91093d73ac848bd43e24baacb69aa6fbdabca46
davidcoderistov <davidcoderistov@gmail.com>
2022-09-15 13:55:15 +0200

    Create ConnectWalletModal->TermsView component

---
+++ src/components/ConnectWalletModal/TermsView.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+
+
+export default function TermsView () {
+
+    return (
+        <Box
+            borderRadius='15px'
+            bgcolor='#212429'
+            padding='14px'
+            marginX='20px'
+        >
+            <Typography fontSize={12}>
+                By connecting a wallet, you agree to Monoswap's Terms of Service and acknowledge that you have read
+                and understand the 1inch Aggregation Protocol.
+            </Typography>
+        </Box>
+    )
+}
