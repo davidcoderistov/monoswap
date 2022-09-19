369eeb9f28c172b25e2e35beae790536a217438b
davidcoderistov <davidcoderistov@gmail.com>
2022-09-19 11:37:27 +0200

    Change global font and add context variables

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import React, {useState} from 'react'
 import { Box } from '@mui/material'
 import MainApp from './components/App'
 import AppContext, { Provider, Account, Wallet } from './context'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+    typography: {
+        allVariants: {
+            fontFamily: 'Verdana',
+        },
+    },
+})
 
 
 function App() {
 
     const [provider, setProvider] = useState<Provider>(null)
     const [selectedAccount, setSelectedAccount] = useState<Account>(null)
+    const [selectedChainId, setSelectedChainId] = useState(1)
     const [connectedTo, setConnectedTo] = useState<Wallet>(null)
+    const [message, setMessage] = useState<string | null>(null)
 
     // Metamask info
     const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false)
@@ -20,8 +31,12 @@ function App() {
         setProvider,
         selectedAccount,
         setSelectedAccount,
+        selectedChainId,
+        setSelectedChainId,
         connectedTo,
         setConnectedTo,
+        message,
+        setMessage,
         metamask: {
             isInstalled: isMetamaskInstalled,
             setIsInstalled: setIsMetamaskInstalled,
@@ -33,24 +48,26 @@ function App() {
     }
 
   return (
-      <AppContext.Provider value={context}>
-          <Box sx={{ padding: 2 }}>
-              <Box sx={{
-                  position: 'fixed',
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  pointerEvents: 'none',
-                  width: '200vw',
-                  height: '200vh',
-                  background: 'radial-gradient(50% 50% at 50% 50%, #fc077d10 0%, rgba(255, 255, 255, 0) 100%)',
-                  transform: 'translate(-50vw, -100vh)',
-                  zIndex: -1,
-                  backgroundColor: '#212429',
-              }} />
-              <MainApp />
-          </Box>
-      </AppContext.Provider>
+      <ThemeProvider theme={theme}>
+          <AppContext.Provider value={context}>
+              <Box sx={{ padding: 2 }}>
+                  <Box sx={{
+                      position: 'fixed',
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      pointerEvents: 'none',
+                      width: '200vw',
+                      height: '200vh',
+                      background: 'radial-gradient(50% 50% at 50% 50%, #fc077d10 0%, rgba(255, 255, 255, 0) 100%)',
+                      transform: 'translate(-50vw, -100vh)',
+                      zIndex: -1,
+                      backgroundColor: '#212429',
+                  }} />
+                  <MainApp />
+              </Box>
+          </AppContext.Provider>
+      </ThemeProvider>
   )
 }
 
