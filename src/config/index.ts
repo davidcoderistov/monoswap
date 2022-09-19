f6a7f62e333fff4fae1f0f2d6eb943c649f8959d
davidcoderistov <davidcoderistov@gmail.com>
2022-09-19 11:34:49 +0200

    Add global configuration file

---
+++ src/config/index.ts
@@ -0,0 +1,37 @@
+
+export const NETWORKS = {
+    ETHEREUM: {
+        MAINNET: 'Ethereum',
+        ROPSTEN: 'Ropsten',
+        RINKEBY: 'Rinkeby',
+        GORLI: 'Gorli',
+        KOVAN: 'Kovan',
+        SEPOLIA: 'Sepolia',
+    },
+    POLYGON: {
+        MAINNET: 'Polygon',
+        MUMBAI: 'Mumbai',
+    },
+    AVALANCHE: {
+        MAINNET: 'Avalanche',
+        FUJI: 'Fuji',
+    },
+    OPTIMISM: {
+        MAINNET: 'Optimism',
+        GORLI: 'Optimism Gorli',
+        KOVAN: 'Optimism Kovan',
+    },
+    ARBITRUM: {
+        MAINNET: 'Arbitrum',
+        RINKEBY: 'Arbitrum Rinkeby',
+        GORLI: 'Arbitrum Gorli',
+    }
+}
+
+export const IMAGES = {
+    [NETWORKS.ETHEREUM.MAINNET]: '/ethereumlogo.png',
+    [NETWORKS.POLYGON.MAINNET]: '/polygonlogo.png',
+    [NETWORKS.AVALANCHE.MAINNET]: '/avalanchelogo.png',
+    [NETWORKS.OPTIMISM.MAINNET]: '/optimismlogo.png',
+    [NETWORKS.ARBITRUM.MAINNET]: 'https://arbiscan.io/images/svg/brands/arbitrum.svg?v=1.3',
+}
