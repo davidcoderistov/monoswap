b07ac120dd01bcf255a7c151c5c2536fa5a5101f
davidcoderistov <davidcoderistov@gmail.com>
2022-09-19 11:36:59 +0200

    Handle network change

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Box } from '@mui/material'
+import Snackbar from "../Snackbar";
 import SelectNetwork from '../SelectNetwork'
 import ConnectWalletButton from '../ConnectWalletButton'
 import AccountButton from '../AccountButton'
 import ConnectWalletModal from '../ConnectWalletModal'
+import SwapInterface from '../SwapInterface'
 import AppContext from '../../context'
 import detectEthereumProvider from '@metamask/detect-provider'
+import { getNetwork, getChainId, getRpcUrls, getNetworkImgSrc, getChainIdHexValue } from '../../utils'
 import { ethers } from 'ethers'
 
 
@@ -16,11 +19,19 @@ export default function App () {
         setProvider,
         selectedAccount,
         setSelectedAccount,
+        selectedChainId,
+        setSelectedChainId,
+        connectedTo,
         setConnectedTo,
+        message,
+        setMessage,
         metamask,
     } = useContext(AppContext)
 
     const [connectWalletOpen, setConnectWalletOpen] = useState(false)
+
+    const selectedNetwork = getNetwork(selectedChainId)
+    const selectedImgSrc = getNetworkImgSrc(selectedChainId)
 
     useEffect( () => {
         const trySetProvider = async () => {
@@ -61,8 +72,8 @@ export default function App () {
             }
             rpcProvider.on('accountsChanged', handleAccountsChange)
 
-            const handleChainChanged = () => {
-                console.log('chain changed')
+            const handleChainChanged = (chainId: string) => {
+                setSelectedChainId(Number(chainId))
             }
             rpcProvider.on('chainChanged', handleChainChanged)
 
@@ -71,7 +82,76 @@ export default function App () {
                 rpcProvider.off('chainChanged', handleChainChanged)
             }
         }
-    }, [provider, setSelectedAccount, setConnectedTo])
+    }, [provider, setSelectedAccount, setConnectedTo, setSelectedChainId])
+
+    useEffect(() => {
+        if (provider && connectedTo === 'metamask') {
+            const trySetNetwork = async () => {
+                try {
+                    const chainId: string = await provider.send('net_version', [])
+                    setSelectedChainId(Number(chainId))
+                } catch (e) {
+                    console.error(e)
+                }
+            }
+            trySetNetwork()
+        }
+    }, [provider, connectedTo, setSelectedChainId])
+
+    const handleChangeSelectedNetwork = (network: string) => {
+        const chainId = getChainId(network)
+        if (chainId) {
+            if (connectedTo === 'metamask') {
+                if (provider) {
+                    const { provider: rpcProvider }: any = provider
+                    if (rpcProvider && rpcProvider.request) {
+                        const trySwitchNetwork = async () => {
+                            try {
+                                await rpcProvider.request({
+                                    method: 'wallet_switchEthereumChain',
+                                    params: [{ chainId: getChainIdHexValue(chainId) }]
+                                })
+                                setSelectedChainId(chainId)
+                            } catch (switchError: any) {
+                                if (switchError.code === 4902) {
+                                    try {
+                                        const rpcUrls = getRpcUrls(chainId)
+                                        await rpcProvider.request({
+                                            method: 'wallet_addEthereumChain',
+                                            params: [
+                                                {
+                                                    chainId: getChainIdHexValue(chainId),
+                                                    chainName: network,
+                                                    rpcUrls,
+                                                },
+                                            ],
+                                        })
+                                    } catch (addError) {
+                                        setMessage(
+                                            `Failed to switch networks from the Monoswap Interface.
+                                             In order to use Monoswap on ${network}, you must change 
+                                             the networks in your wallet.`
+                                        )
+                                        return
+                                    }
+                                }
+                                setMessage(
+                                    `Failed to switch networks from the Monoswap Interface.
+                                             In order to use Monoswap on ${network}, you must change 
+                                             the networks in your wallet.`
+                                )
+                            }
+                        }
+                        trySwitchNetwork()
+                    }
+                }
+            } else if (connectedTo === 'walletconnect') {
+                // TODO: Handle network change when connected to WalletConnect
+            } else {
+                setSelectedChainId(chainId)
+            }
+        }
+    }
 
     const handleOpenConnectWallet = () => {
         setConnectWalletOpen(true)
@@ -81,25 +161,55 @@ export default function App () {
         setConnectWalletOpen(false)
     }
 
+    const handleCloseSnackbar = () => {
+        setMessage(null)
+    }
+
     return (
-        <Box
-            component='div'
-            display='flex'
-            justifyContent='end'
-        >
-            <SelectNetwork />
-            <Box ml='12px' />
-            { selectedAccount ? (
-                <AccountButton
-                    address={selectedAccount}
-                    onClick={handleOpenConnectWallet} />
-            ) : (
-                <ConnectWalletButton onClick={handleOpenConnectWallet} />
-            )}
-            <ConnectWalletModal
-                open={connectWalletOpen}
-                onClose={handleCloseConnectWallet}
-            />
-        </Box>
+        <React.Fragment>
+            <Box
+                component='div'
+                display='flex'
+                justifyContent='end'
+            >
+                { selectedNetwork && (
+                    <SelectNetwork
+                        selectedNetwork={selectedNetwork}
+                        selectedImgSrc={selectedImgSrc}
+                        onChange={handleChangeSelectedNetwork}
+                    />
+                )}
+                <Box ml='12px' />
+                { selectedAccount ? (
+                    <AccountButton
+                        address={selectedAccount}
+                        onClick={handleOpenConnectWallet} />
+                ) : (
+                    <ConnectWalletButton onClick={handleOpenConnectWallet} />
+                )}
+                <ConnectWalletModal
+                    open={connectWalletOpen}
+                    onClose={handleCloseConnectWallet}
+                />
+            </Box>
+            <Box
+                component='div'
+                display='flex'
+                justifyContent='end'
+            >
+                <Snackbar
+                    open={Boolean(message)}
+                    message={message ?? ''}
+                    onClose={handleCloseSnackbar} />
+            </Box>
+            <Box
+                component='div'
+                display='flex'
+                justifyContent='center'
+                marginTop='80px'
+            >
+                <SwapInterface />
+            </Box>
+        </React.Fragment>
     )
 }
