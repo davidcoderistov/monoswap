19856e7e485e9ee81e008c22a121c76e8d607866
davidcoderistov <davidcoderistov@gmail.com>
2022-09-19 12:50:15 +0200

    Swap ActionButton component with new one in ConnectWalletModal

--- src/components/ConnectWalletModal/ConnectWalletModalTitle.tsx
+++ src/components/ConnectWalletModal/ConnectWalletModalTitle.tsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import { Box, IconButton, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { Close, ArrowBack } from '@mui/icons-material'
+import ActionButton from '../ActionButton'
 
-
-const ActionButton = (props: { disabled: boolean, onClick: () => void, children: React.ReactNode }) => {
-
-    return (
-        <IconButton
-            sx={{ color: '#FFFFFF', p:0, ':disabled': { color: '#9f9f9f' }}}
-            disabled={props.disabled}
-            onClick={props.onClick}
-        >
-            { props.children }
-        </IconButton>
-    )
-}
 
 interface ConnectWalletModalTitleProps {
     title: string
