dc8835b2706b5aa2ed07e545cee79a12dd023612
davidcoderistov <davidcoderistov@gmail.com>
2022-09-19 11:30:15 +0200

    Create initial version of SwapInterface component

---
+++ src/components/SwapInterface/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+import TokenInput from './TokenInput'
+import ArrowDownward from './ArrowDownward'
+import ConnectWalletButton from './ConnectWalletButton'
+
+
+export default function SwapInterface () {
+
+    return (
+        <Box
+            component='div'
+            bgcolor='#191B1F'
+            padding='10px'
+            borderRadius='20px'
+            width='464px'
+        >
+            <Box
+                component='div'
+                marginLeft='10px'
+                marginTop='5px'
+                marginBottom='10px'
+            >
+                <Typography color='#FFFFFF'>
+                    Swap
+                </Typography>
+            </Box>
+            <TokenInput
+                value=''
+                onChange={() => {}}
+                active={true}
+                symbol='Eth'
+                imgSrc='/ethereumlogo.png'
+                onClick={() => {}} />
+            <ArrowDownward active={false} onClick={() => {}} />
+            <TokenInput
+                value=''
+                onChange={() => {}}
+                active={false}
+                symbol='Eth'
+                imgSrc='/ethereumlogo.png'
+                onClick={() => {}} />
+            <ConnectWalletButton onClick={() => {}} />
+        </Box>
+    )
+}
