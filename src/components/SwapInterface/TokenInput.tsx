d871e61356904f14124ed41813cdd046dd7ef82c
davidcoderistov <davidcoderistov@gmail.com>
2022-09-29 10:46:18 +0200

    Add balance placeholder in TokenInput component

--- src/components/SwapInterface/TokenInput.tsx
+++ src/components/SwapInterface/TokenInput.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback } from 'react'
-import { Box, InputBase } from '@mui/material'
+import { Box, InputBase, Typography } from '@mui/material'
 import Token, { TokenProps } from '../Token'
 
 
 export type TokenInputProps = {
     value: string
     onChange: (value: string) => void
+    balance?: string
 } & TokenProps
 
 
-export default function TokenInput ({ value, onChange, type, symbol, imgSrc, onClick }: TokenInputProps) {
+export default function TokenInput ({ value, onChange, balance, type, symbol, imgSrc, onClick }: TokenInputProps) {
 
     const handleChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         onChange(event.target.value)
@@ -19,44 +20,69 @@ export default function TokenInput ({ value, onChange, type, symbol, imgSrc, onC
         <Box
             component='div'
             display='flex'
-            flexDirection='row'
-            alignItems='center'
-            columnGap='10px'
+            flexDirection='column'
             paddingX='16px'
             paddingTop='16px'
-            paddingBottom={type === 'select' ? '40px' : '16px'}
+            paddingBottom='8px'
             bgcolor='#212429'
             borderRadius='20px'
             border='1px solid #191B1F'
             sx={{ '&:hover': { border: '1px solid #40444F' } }}
         >
             <Box
-                flex='1 1 auto'
-            >
-                <InputBase
-                    sx={{
-                        '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
-                            display: 'none',
-                        },
-                        '& input[type=number]': {
-                            MozAppearance: 'textfield'
-                        },
-                        color: '#FFFFFF',
-                        font: '32px monospace'
-                    }}
-                    value={value}
-                    onChange={handleChange}
-                    type='number'
-                    placeholder='0.0'
-                    fullWidth
+                component='div'
+                display='flex'
+                flexDirection='row'
+                alignItems='center'
+                columnGap='10px'
+                >
+                <Box
+                    flex='1 1 auto'
+                >
+                    <InputBase
+                        sx={{
+                            '& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button': {
+                                display: 'none',
+                            },
+                            '& input[type=number]': {
+                                MozAppearance: 'textfield'
+                            },
+                            color: '#FFFFFF',
+                            font: '32px monospace'
+                        }}
+                        value={value}
+                        onChange={handleChange}
+                        type='number'
+                        placeholder='0.0'
+                        fullWidth
+                    />
+                </Box>
+                <Token
+                    type={type}
+                    symbol={symbol}
+                    imgSrc={imgSrc}
+                    onClick={onClick}
                 />
             </Box>
-            <Token
-                type={type}
-                symbol={symbol}
-                imgSrc={imgSrc}
-                onClick={onClick}
-            />
+            { type === 'select' && (
+                <Box
+                    component='div'
+                    display='flex'
+                    flexDirection='row'
+                    justifyContent='end'
+                    alignItems='center'
+                    height={24}
+                >
+                    { balance && (
+                        <Typography
+                            color='#8F96AC'
+                            sx={{ font: '16px monospace', cursor: 'pointer' }}
+                        >
+                            Balance: { balance }
+                        </Typography>
+                    )}
+                </Box>
+            )}
         </Box>
     )
 }
