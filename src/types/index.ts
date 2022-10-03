6adb7397441571529987c611a82a75fda553f3c6
davidcoderistov <davidcoderistov@gmail.com>
2022-10-03 12:09:17 +0200

    Add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,32 @@
+
+
+export interface BridgeTokenI {
+    tokenAddress: string
+}
+
+export interface BridgeInfoI {
+    '10'?: BridgeTokenI
+    '137'?: BridgeTokenI
+    '42161'?: BridgeTokenI
+}
+
+export interface ExtensionsI {
+    bridgeInfo: BridgeInfoI
+}
+
+export interface TokenI {
+    name: string
+    symbol: string
+    address: string
+    decimals: number
+    chainId: number
+    logoURI: string
+    extensions?: ExtensionsI
+}
+
 export interface Token {
     id: string
     address: string
-    blockchain: string
     name: string
     symbol: string
     thumbnail: string
