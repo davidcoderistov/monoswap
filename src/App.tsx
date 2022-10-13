3fb297b4ee7a0e966fe01fe15e67903459557f9c
davidcoderistov <davidcoderistov@gmail.com>
2022-10-13 13:43:48 +0200

    Change background color

--- src/App.tsx
+++ src/App.tsx
@@ -83,7 +83,7 @@ function App() {
                       pointerEvents: 'none',
                       width: '200vw',
                       height: '200vh',
-                      background: 'radial-gradient(50% 50% at 50% 50%, #fc077d10 0%, rgba(255, 255, 255, 0) 100%)',
+                      background: 'radial-gradient(50% 50% at 50% 50%, #130f4099 0%, rgba(255, 255, 255, 0) 100%)',
                       transform: 'translate(-50vw, -100vh)',
                       zIndex: -1,
                       backgroundColor: '#212429',
